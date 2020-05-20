cb9b0b78b713605329572d6e3213b01ad2c8a2ea
jussiniinikoski <jussi.niinikoski@perjantai.fi>
2020-05-20 16:32:55 +0300

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: 'index.html'},
+        {from: 'text-example.json'},
+        {from: 'pizza.jpg'},
+      ],
+    }),
+  ],
   mode: "development"
 };
